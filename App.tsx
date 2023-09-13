03ff0b834d9712fda0df8fbc692f42d2124e4da7
BERNARDO MACHADO <bernardo_machado1@estudante.sesisenai.org.br>
2023-09-13 12:48:51 -0300

    jamal

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = React.useState
+  const [genero, setGenero] = React.useState
+  const [dataNascimento, setNascimento] = React.useState
+  const [email, setEmail] = React.useState
+  const [idiomaCurriculo, setIdioma] = React.useState
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Nome' style={styles.input}></TextInput>
+      <TextInput placeholder='Genero' style={styles.input}></TextInput>
+      <TextInput placeholder='Data de nascimento' style={styles.input}></TextInput>
+      <TextInput placeholder='Usuário'style={styles.input}></TextInput>
+      <TextInput placeholder='E-mail' style={styles.input}></TextInput>
+      <TextInput placeholder='Senha' style={styles.input}></TextInput>
+      <TextInput placeholder='Confirme a senha' style={styles.input}></TextInput>
+      <TextInput placeholder='CPF'style={styles.input}></TextInput>
+      <TextInput placeholder='Idioma do Currículo' style={styles.input}></TextInput>
+      
+      <TouchableOpacity style={styles.button}><Text style={{color: "white"}}>Cadastrar</Text></TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +33,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input:{
+    marginBottom: 30,
+    borderBottomWidth: 1,
+    width: 200,
+    height: 30
+
+  },
+  button:{
+    display: 'flex',
+    width: 200,
+    height: 50,
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: "#4b98b8",
+  
   },
 });
